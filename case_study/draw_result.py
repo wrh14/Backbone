78414fd4511d8be0bf2f99bb334412950ad29e99
Ruihan Wu <ruihan.wu14@gmail.com>
2017-09-15 09:28:31 -0400

    Modify draw_result.py for venue

--- case_study/draw_result.py
+++ case_study/draw_result.py
@@ -14,7 +14,7 @@ pos = {}
 
 
 # f2 = open('../../Output/citation_data_mining/case_study_M/final_ifCS=0_k=10_alpha=0.9_N=100_M=10_ifSub=1_ifDelta=1','rb')
-f2 = open('../../Output/citation_data_mining/baseline/page_rank_final_size=100_alpha=0.9_threshold=1e-15','rb')
+f2 = open('../../Output/venue/case_study_M/final_ifCS=0_k=20_alpha=0.9_N=20_M=10_ifSub=1_ifDelta=1','rb')
 line = f2.readline()
 line = f2.readline()
 while line :
@@ -22,44 +22,46 @@ while line :
 	line = f2.readline()
 	raw_top_nodes[int(row[0])] = 'top_list'
 f2.close()
-print('finish reading positions')
 
-f1 = open('../../emb/citation_data_mining/data_mining_indexNetwork.txt','rb')
+f1 = open('../../emb/venue/venue_indexNetworkMin10.txt','rb')
 line = f1.readline()
 while line:
 	row = line.split(' ')
 	line = f1.readline()
-
-r = 100
-
-f1 = open('../../emb/citation_data_mining/data_mining_indexNetwork.txt','rb')
-line = f1.readline()
-while line:
-	row = line.split(' ')
-	line = f1.readline()
-	if random.uniform(0,1) < 1:
-		if abs(float(row[1]) + 5) < r and abs(float(row[2]) + 2) < r - 1:
-			pos[int(row[0])] = (float(row[1]), float(row[2]))
-			G.add_node(int(row[0]))
-	else:
-		if raw_top_nodes.get(int(row[0])) == 'top_list':
-			pos[int(row[0])] = (float(row[1]), float(row[2]))
-			G.add_node(int(row[0]))
+	pos[int(row[0])] = (float(row[1]), float(row[2]))
 f1.close()
 
-print('finish reading nodes')
+f1 = open('../../Dataset/venue/indexNetwork.txt','rb')
+line = f1.readline()
+while line:
+	row = line.split(' ')
+	if pos.has_key(int(row[0])) and pos.has_key(int(row[1])) and int(row[2]) >= 16:
+		G.add_edge(int(row[0]), int(row[1]))
+	line = f1.readline()
+f1.close()
+
+labels = {}
+f1 = open('../../Dataset/venue/indexToName.txt','rb')
+line = f1.readline()
+while line:
+	row = line.split(' ')
+	labels[int(row[0])] = row[1].split('\t')[1]
+	line = f1.readline()
+f1.close()
 
 
-plt.figure(figsize = (10,10), dpi = 100)
-nx.draw(G, pos, node_color = 'grey', node_size = 5, alpha = 0.5)
+plt.figure(figsize = (10,10), dpi = 500)
+nx.draw(G, pos, node_color = 'grey', edge_color = 'grey', arrows=False, node_size = 1, alpha = 0.3)
 
 G2 = nx.DiGraph()
 
+top_labels = {}
 for i in raw_top_nodes:
 	G2.add_node(i)
+	top_labels[i] = labels[i]
 
 print(len(G2.nodes()))
-nx.draw(G2, pos, node_color = 'red', node_size = 20, alpha = 1)
+nx.draw(G2, pos, labels=top_labels, node_color = 'red', node_size = 3, alpha = 1, font_size=10)
 
 # plt.savefig('backbone_citation-DM')
-plt.savefig('page_rank_citation-DM')
+plt.savefig('backbone_venue')
